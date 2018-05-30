4ff096590a928102e45d0d3841ec5eef1df86905
Waldo Lavaut <w86compositor@gmail.com>
2018-05-30 14:38:07 -0400

    Starting the app

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { format } from "url";
+
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <form className="store-selector">
+                <h2>Please Enter a Store</h2>
+                <input type="text" required placeholder="Store Name"/>
+                <button type="submit">Visit Store -> </button>
+            </form>
+        )
+    }
+}
+
+
+export default StorePicker;
