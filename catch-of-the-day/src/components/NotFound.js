8838834567b12c5eb5ff5644c109439b1581d63f
Waldo Lavaut <w86compositor@gmail.com>
2018-06-01 15:00:50 -0400

    Working on this...finished the lesson Routing with React Router, on to Helper and Utility Functions now

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+  <h2>Not found!?!!!!111</h2>
+  </div>
+)
+
+
+export default NotFound;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { format } from "url";
 
-
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <form className="store-selector">
-                <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name"/>
-                <button type="submit">Visit Store -> </button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -> </button>
+      </form>
+    );
+  }
 }
-
 
 export default StorePicker;
